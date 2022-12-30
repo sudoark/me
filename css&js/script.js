6478a54a244715c95d2721527acfa19ba93a4d57
ARKINUX <88921403+sudoark@users.noreply.github.com>
2022-12-30 16:47:30 +0530

    Add files via upload

---
+++ css&js/script.js
@@ -0,0 +1,12 @@
+VanillaTilt.init(document.querySelectorAll(".social__links ul li"), {
+    max: 10,
+    speed: 10,
+    glare: true,
+    "max-glare": 1
+});
+VanillaTilt.init(document.querySelectorAll(".payment ul li"), {
+    max: 10,
+    speed: 10,
+    glare: true,
+    "max-glare": 1
+});
